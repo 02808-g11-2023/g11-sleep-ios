24918f937b883c46ad337e8999a0f6aa88f0fae0
Alexander Johansson <alexander@shape.dk>
2023-04-19 22:03:49 +0200

    Adds Feedback page and a start to Charts

--- g11-sleep-ios/ContentView.swift
+++ g11-sleep-ios/ContentView.swift
@@ -5,44 +5,80 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
     @State private var showFeedbackSheet = false
+    @State private var timelineSelection: Timeline = .day
     
     @EnvironmentObject var vm: ContentViewModel
     
     var body: some View {
-        TabView {
-            NavigationView {
-                Text("Steps today: \(vm.userStepCount)")
-                    .navigationTitle("Hi Alexander!")
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button {
-                                showFeedbackSheet.toggle()
-                            } label: {
-                                Image(systemName: "plus")
-                            }
-                            .sheet(isPresented: $showFeedbackSheet) {
-                                FeedbackSheet()
-                            }
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Picker("Timeline", selection: $timelineSelection) {
+                        Text("Day").tag(Timeline.day)
+                        Text("Week").tag(Timeline.week)
+                        Text("Month").tag(Timeline.month)
+                        Text("Year").tag(Timeline.year)
+                    }
+                    .pickerStyle(.segmented)
+                    Chart {
+                        BarMark(
+                            x: .value("Name", "Awake"),
+                            y: .value("Value", 10)
+                        )
+                        BarMark(
+                            x: .value("Name", "REM"),
+                            y: .value("Value", 70)
+                        )
+                        BarMark(
+                            x: .value("Name", "Core"),
+                            y: .value("Value", 40)
+                        )
+                        BarMark(
+                            x: .value("Name", "Deep"),
+                            y: .value("Value", 30)
+                        )
+                    }
+                    .frame(height: 275)
+                    Chart {
+                        BarMark(
+                            x: .value("Value", 10),
+                            y: .value("Name", "Awake")
+                        )
+                        BarMark(
+                            x: .value("Value", 70),
+                            y: .value("Name", "REM")
+                        )
+                        BarMark(
+                            x: .value("Value", 40),
+                            y: .value("Name", "Core")
+                        )
+                        BarMark(
+                            x: .value("Value", 30),
+                            y: .value("Name", "Deep")
+                        )
+                    }
+                    .frame(height: 275)
+                }
+            }
+                .padding(20)
+                .navigationTitle("Hi Alexander!")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showFeedbackSheet.toggle()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .sheet(isPresented: $showFeedbackSheet) {
+                            FeedbackSheet()
                         }
                     }
-            }
-            .tabItem {
-                Image(systemName: "heart.fill")
-                Text("Insights")
-            }
-            NavigationView {
-                Text("Content here")
-                    .navigationTitle("Settings")
-            }
-            .tabItem {
-                Image(systemName: "person.fill")
-                Text("Profile")
-            }
+                }
         }
-        
     }
     
 }
--- g11-sleep-ios/FeedbackSheet.swift
+++ g11-sleep-ios/FeedbackSheet.swift
@@ -9,9 +9,145 @@ import SwiftUI
 struct FeedbackSheet: View {
     @Environment(\.dismiss) var dismiss
     
+    @State private var date = Date()
+    @State private var textInput: String = ""
+    
     var body: some View {
         NavigationView {
-            Text("Content")
+            Form {
+                DatePicker(
+                    "Feedback for",
+                    selection: $date,
+                    in: ...Date(),
+                    displayedComponents: [.date]
+                )
+                .datePickerStyle(.automatic)
+                .padding(1)
+                
+                Section(header: Text("Sleep Quality")) {
+                    HStack(alignment: .center) {
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "1.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.red)
+                        //.opacity(0.1)
+                        
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "2.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.orange)
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "3.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.yellow)
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "4.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.cyan)
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "5.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.green)
+                    }
+                }
+                Section(header: Text("Exercise Quality")) {
+                    HStack(alignment: .center) {
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "1.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.red)
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "2.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.orange)
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "3.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.yellow)
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "4.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.cyan)
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "5.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.green)
+                    }
+                }
+                
+                Section(header: Text("Optional input")) {
+                    TextField("How did you feel?", text: $textInput)
+                        .frame(height: 275, alignment: .top)
+                }
+            }
                 .toolbar {
                     ToolbarItem(placement: .cancellationAction) {
                         Button("Cancel") {
